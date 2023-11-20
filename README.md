e77d34906f8ece5e3bec5b9b25e2b9abbc206900
dyson <hrkedz@gmail.com>
2023-11-20 09:27:34 +0900

    seed作ってREADMEに環境構築方法書いた

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
 * Ruby version
+3.2.2
 
-* System dependencies
+* セットアップ
+bin/rails db:setup
 
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+* 起動
+bin/rails s
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.find_or_create_by(name: '山田')
+User.find_or_create_by(name: '鈴木')
+User.find_or_create_by(name: '佐藤')
+User.find_or_create_by(name: '高橋')
+User.find_or_create_by(name: '斉藤')
+
+Category.find_or_create_by(name: 'Ruby')
+Category.find_or_create_by(name: 'Python')
+Category.find_or_create_by(name: 'GO')
+Category.find_or_create_by(name: 'PHP')
+
+Article.find_or_create_by(title: '記事1です', content: '記事1です', user: User.all.sample).tap do |article|
+  article.category_ids = Category.all.ids.sample(2)
+end
+
+Article.find_or_create_by(title: '記事2です', content: '記事2です', user: User.all.sample).tap do |article|
+  article.category_ids = Category.all.ids.sample(2)
+end
